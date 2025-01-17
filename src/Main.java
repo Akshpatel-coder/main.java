97e66b33ddffe6d37fc8ac7f73dce42a0172a731
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-17 19:47:33 +0530

    lesser or greater

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 
+        import java.util.*;
+        public class Main {
 
-    import java.util.*;
-
-    public class Main {
-      public static void main (String[] args) {
+        public static void main(String args[]) {
             Scanner sc = new Scanner(System.in);
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int sum = a - b;
-          System.out.println(sum);
+
+            if(a == b) {
+                System.out.println("Equal");
+            } else {
+                if(a > b) {
+                    System.out.println("a is greater");
+                } else {
+                    System.out.println("a is lesser");
+                }
+            }
         }
     }
