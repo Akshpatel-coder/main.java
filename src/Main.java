546e668f103177d76babb68b549792b201ae8fb5
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-18 13:25:03 +0530

    loops (for loop , while loop , do while loop)

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
+import java.util.*;
+   public class Main {
 
-        import java.util.*;
-        public class Main {
+       public static void main(String args[]) {
+           Scanner sc = new Scanner(System.in);
+           int n = sc.nextInt();
 
-        public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+           int sum = 0;
+           for (int i = 1; i <11; i++) {
+               System.out.println(i*n);
+           }
+       }
+   }  //counter++ = > counter = counter + 1
+           //for (int counter = 0; counter < 11; counter++) {
+               //System.out.println(counter + " ");
+           //
+           //for (int i = 0; i < 11; i++) {
+              // System.out.println(i);
 
-            if(a == b) {
-                System.out.println("Equal");
-            } else {
-                if(a > b) {
-                    System.out.println("a is greater");
-                } else {
-                    System.out.println("a is lesser");
-                }
-            }
-        }
-    }
+               //int i = 0;
+               //while (i < 11) {
+                   //System.out.println(i);
+                   //i = i + 1;  // i++;
+               //}
+               //int i = 0;
+               //do {
+                 //  System.out.println(i);
+                  // i = i + 1;
+               //} while (i < 11);
+
+           //}
+       //}
