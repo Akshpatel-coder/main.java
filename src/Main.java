9be6d7b2b596ab8987b2f6bb2a36df112e8bb133
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-21 19:40:54 +0530

    patterns

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,43 @@
 
      public static void main(String args[]) {
          int n = 5;
-         int number = 1;
-         //outer loop
-         for (int i = 1; i <= n; i++) {
-             //inner loop
-             //for (int j= 1; j<=i; j++) {
-             //System.out.print("*");
 
+         //upper half
+         for (int i = 1; i <= n; i++) {
+             //1st part
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("$");
+             }
+             //spaces
+             int spaces = 2 * (n - i);
+             for (int j = 1; j <= spaces; j++) {
+                 System.out.print(" ");
+
+             }
+             //2nd part
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("$");
+             }
+             System.out.println();
          }
-         //System.out.println();
-         //outer loop
-         for (int i=1; i<=n; i++) {
-             //inner loop
-             for (int j=1; j<=i; j++) {
-                 System.out.print(number);
-                 number++;//number = number + 1
+         //lower half
+         for (int i = n; i >= 1; i--) {
+             //1st part
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("$");
+             }
+             //spaces
+             int spaces = 2 * (n - i);
+             for (int j = 1; j <= spaces; j++) {
+                 System.out.print(" ");
+
+             }
+             //2nd part
+             for (int j = 1; j <= i; j++) {
+                 System.out.print("$");
              }
              System.out.println();
          }
 
      }
  }
-
