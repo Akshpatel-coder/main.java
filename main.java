093c5cdd37e838d8d500d9771e54663a41a6fa3e
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-22 19:18:01 +0530

    aa

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main {
+
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return 1;
+    }
+    public static void main(String args[]) {
+
+    }
+}
+}
