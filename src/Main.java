0defa4aef2ac9ecf5b6b0cad68d08a4637f86dac
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-24 20:00:21 +0530

    1st pattern

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,1 @@
-import java.util.*;
 
-public class Main {
-
-   public static void printMyName(String name) {
-      System.out.println(name);
-      return ;
-   }
-   public static void main(String args[]) {
-      Scanner sc=new Scanner();
-      String name = sc.next();
-
-      printMyName(name);//call kiya function ko
-
-   }
-}
