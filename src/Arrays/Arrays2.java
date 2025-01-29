83e2421250914b2947ea66046e0634f8a701164f
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-29 19:14:13 +0530

    arrays

---
+++ src/Arrays/Arrays2.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Arrays2 {
+
+    public static <Scanner, Scanner> void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.notify();
+        int number[] = new int[size];
+        
+        for(int i=0; i<size; i++) {
+            System.out.println(number[i]);
+        }
+    }
+}
