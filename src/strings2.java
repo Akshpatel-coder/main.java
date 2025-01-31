59a6dfdd4715fc37d955231357b482a1d793b991
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-31 19:10:55 +0530

    String

---
+++ src/strings2.java
@@ -0,0 +1,25 @@
+public class strings2 {
+    public static void main(String args[]) {
+
+        String name1 = "Peter";
+        String name2 = "Peter";
+
+        //1 s1 > s2 : +ve value
+        //2 s1 == s2 : 0
+        //3 s1 < s2 : -ve value
+
+
+        //if(name1.compareTo(name2)== 0) {
+        //  System.out.println("Strings are equal");
+        //} else {
+        //  System.out.println("Strings are not equal");
+        //}
+        //}
+        if (new String("Tony") == new String("Tony")) {
+            System.out.println("String are equl");
+        } else {
+            System.out.println("Strings are  Not equal");
+        }
+
+    }
+}
