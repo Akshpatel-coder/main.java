002b506938d1e41f453c23c6e5cfd010531a97a6
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-01-31 19:13:49 +0530

    stringsss

---
+++ src/string.java
@@ -0,0 +1,11 @@
+public class string {
+
+    public static void main(String args[]) {
+
+        String sentence = "TonyStark";
+        String name = sentence.substring(4);
+        System.out.println(name);
+
+        //strings are Immutable
+    }
+}
