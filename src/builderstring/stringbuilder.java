68e35532f984efc67067e813df21bc10b8228b2a
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-02-01 12:25:41 +0530

    stringbuilders

---
+++ src/builderstring/stringbuilder.java
@@ -0,0 +1,18 @@
+package builderstring;
+
+
+public class stringbuilder {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        sb.append("W");
+        sb.append("o");
+        sb.append("r");
+        sb.append("l");
+        sb.append("d");
+        System.out.println(sb.length());
+    }
+}
