6773ddf22ffcd7011e1e3acc6cc67dbfeef74d0f
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-02-03 20:45:28 +0530

    newstring

---
+++ src/strings3.java
@@ -0,0 +1,24 @@
+public class strings3 {
+        public static void main(String[] args) {
+            // Declare and initialize a string
+            String greeting = "Hello, World!";
+
+            // Print the string
+            System.out.println(greeting);
+
+            // String concatenation
+            String name = "Alice";
+            String welcomeMessage = greeting + " Welcome, " + name + "!";
+            System.out.println(welcomeMessage);
+
+            // Get the length of the string
+            int length = greeting.length();
+            System.out.println("Length of the greeting: " + length);
+
+            // Access a character at a specific index
+            char charAtPosition = greeting.charAt(7);
+            System.out.println("Character at index 7: " + charAtPosition);
+        }
+    }
+
+
