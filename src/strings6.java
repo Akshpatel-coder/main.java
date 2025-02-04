54e9332d4ba253fa38bd988c84cee59e4879d9df
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-02-04 19:43:37 +0530

    string

---
+++ src/strings6.java
@@ -0,0 +1,28 @@
+public class strings6 {
+
+
+
+        public static void main(String[] args) {
+            // Declare and initialize a string
+            String greeting = "Hello, World!";
+
+            // Print the string
+            System.out.println(greeting);
+
+            // Concatenate two strings
+            String name = "Alice";
+            String welcomeMessage = greeting + " Welcome, " + name + "!";
+            System.out.println(welcomeMessage);
+
+            // Get the length of the string
+            int length = greeting.length();
+            System.out.println("Length of the greeting: " + length);
+
+            // Access a character at a specific index
+            char charAtPosition = greeting.charAt(7);
+            System.out.println("Character at index 7: " + charAtPosition);
+        }
+    }
+
+
+
