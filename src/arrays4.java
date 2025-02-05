3e8bcdf2f70fcd1c8a936943b310c0a8c65a982e
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-02-05 18:27:39 +0530

    newarrays

---
+++ src/arrays4.java
@@ -0,0 +1,24 @@
+public class arrays4 {
+
+        public static void main(String[] args) {
+            // Declare and initialize an array of 3 integers
+            int[] numbers = {10, 20, 30};
+
+            // Print the elements of the array
+            System.out.println("Array elements:");
+            System.out.println("Element 1: " + numbers[0]);
+            System.out.println("Element 2: " + numbers[1]);
+            System.out.println("Element 3: " + numbers[2]);
+
+            // Change the value of the second element (index 1)
+            numbers[1] = 50;
+
+            // Print the updated array
+            System.out.println("\nUpdated array:");
+            System.out.println("Element 1: " + numbers[0]);
+            System.out.println("Element 2: " + numbers[1]);
+            System.out.println("Element 3: " + numbers[2]);
+        }
+    }
+
+
