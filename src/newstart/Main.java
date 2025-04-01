e5b76a6eb1f1fa24f11fca400e2c886d891944c4
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-01 20:44:23 +0530

    newstart

---
+++ src/newstart/Main.java
@@ -0,0 +1,14 @@
+package newstart;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("The sum of these number is:");
+        int num1 = 6;
+        int num2 = 5;
+        int num3 =8;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
