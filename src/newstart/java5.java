4b7bf01ad3457358bff19c328169ff5fc3108dae
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-02 21:50:15 +0530

    java5

---
+++ src/newstart/java5.java
@@ -0,0 +1,15 @@
+package newstart;
+
+public class java5 {
+    public static void main(String[] args) {
+        int a = 4;
+       // int b = 6 % a;
+        int b = 9;
+        b *= 3;
+        System.out.println(b);
+        // System.out.println(64<6);
+        //System.out.println(64>5 && 64>98);
+        System.out.println(64>5 || 64>98);
+        System.out.println(2&3);
+    }
+}
