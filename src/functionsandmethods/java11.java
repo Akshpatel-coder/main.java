fe19b25663b3654e2e40610a54d89b4920bdb4dd
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-04 21:44:44 +0530

    java11

---
+++ src/functionsandmethods/java11.java
@@ -0,0 +1,23 @@
+package functionsandmethods;
+
+import java.util.Scanner;
+
+public class java11 {
+    public static void main(String[] args) {
+      //  String name = new String("Aksh");
+      //  String name = "Aksh";
+      //  System.out.print("the name is: ");
+      //  System.out.print(name);
+
+        //*****************
+        //int a = 6;
+        //float b = 5.6454f;
+       // System.out.printf("The value of a is %d and value of b is %f", a, b);
+        //System.out.format("The value of a is %d and value of b is %f", a, b);
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+
+
+    }
+}
