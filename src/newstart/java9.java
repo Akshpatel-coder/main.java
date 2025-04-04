41fbdc7949112e44653e84283b8df9c75bbce7f0
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-04 21:07:35 +0530

    java9

---
+++ src/newstart/java9.java
@@ -0,0 +1,15 @@
+package newstart;
+
+public class java9 {
+    public static void main(String[] args) {
+         //float a = 7/4.0f * 9/2.0f;
+        //System.out.println(a);
+        char grade = 'B';
+        grade = (char) (grade  + 8);
+        System.out.println(grade);
+
+
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+    }
+}
