3507481bd33aaba773011fbd831b3b124e2b7b4b
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-06 21:40:44 +0530

    java12

---
+++ src/newstart/java12.java
@@ -0,0 +1,44 @@
+package newstart;
+
+import java.sql.SQLOutput;
+
+public class java12 {
+    public static void main(String[] args) {
+        String name = "Aksh";
+      //  System.out.println(name);
+        int value = name.length();
+        //  System.out.println(value);
+     // String lstring = name.toLowerCase();
+       // System.out.println(lstring);
+
+       // String ustring = name.toUpperCase();
+       // System.out.println(ustring);
+
+       // String nonTrimmedString = "          Aksh      ";
+       // System.out.println(nonTrimmedString);
+       // String trimmedString = nonTrimmedString.trim();
+       // System.out.println(trimmedString);
+
+       // System.out.println(name.substring(2));
+      //  System.out.println(name.substring(1,4));
+
+       // System.out.println(name.replace('h', 'n' ));
+       // System.out.println(name.replace("sh", "na" ));
+
+       // System.out.println(name.startsWith("Aks"));
+       // System.out.println(name.endsWith("sh"));
+
+      //  System.out.println(name.charAt(0));
+
+       // String modifiedName = "Akshhh";
+       // System.out.println(modifiedName.indexOf("sh"));
+       // System.out.println(modifiedName.indexOf("hh", 4));
+       // System.out.println(modifiedName.lastIndexOf("hhh" , 4));
+
+       // System.out.println(name.equals("Aksh"));
+        System.out.println(name.equalsIgnoreCase("Aksh"));
+
+        System.out.println(" I am escape sequence \t double quote");
+    }
+
+}
