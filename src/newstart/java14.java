cf692fcb84e3be91af82ea0c95e59aa3aad2fba4
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-07 22:04:07 +0530

    java14

---
+++ src/newstart/java14.java
@@ -0,0 +1,19 @@
+package newstart;
+
+public class java14 {
+
+
+    public static void main(String[] args) {
+        int age = 18;
+        boolean cond =  (age==18);
+        if(cond){
+            System.out.println("Yes boy you can  drive!");
+
+        }
+     //   else {
+     //       System.out.println("No boy you cannot  not drive yet!");
+     //   }
+
+    }
+
+}
