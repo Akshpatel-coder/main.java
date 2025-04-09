7a8acf99e4fd830a4af1f791817ef70762047919
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-09 22:45:18 +0530

    java16

---
+++ src/functionsandmethods/java16.java
@@ -0,0 +1,43 @@
+package functionsandmethods;
+import java.util.Scanner;
+public class java16 {
+    public static void main(String[] args) {
+       String var = "Sourabh";
+
+          switch (var) {
+            case "Shubham"->{
+                System.out.println("You are going to become an Adult");
+               System.out.println("You are going to become an Adult");
+              System.out.println("You are going to become an Adult");
+            }
+              case "Sourabh"->
+                  System.out.println("You are going to join  a Job!");
+              case "Vishaka"->
+                  System.out.println("You are going to get retired!");
+              default ->System.out.println("Enjoy Your Life!");
+          }
+        System.out.println("Thanks for using  my java code!");
+        /*
+         int age;
+        System.out.println("Enter Your Age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+        if (age>56){
+            System.out.println("You are experienced");
+        }
+        else if(age>46){
+            System.out.println("You are semi-experienced!");
+        }
+        else if(age>36) {
+            System.out.println("You are semi-semi experienced!");
+        }
+        else {
+            System.out.println("You are not experienced");
+        }
+        if(age>2){
+            System.out.println("You are not a baby!");
+        }
+
+         */
+    }
+}
