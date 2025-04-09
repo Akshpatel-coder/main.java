0805de660ebbd47c0f69a95c61bc4a0a6ef7d759
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-04-09 21:23:41 +0530

    java15

---
+++ src/newstart/java15.java
@@ -0,0 +1,29 @@
+package newstart;
+
+public class java15 {
+    public static void main(String[] args) {
+        System.out.println("for Logical AND...");
+        boolean a = true;
+        boolean b = false;
+
+    //    if (a  && b ) {
+      //      System.out.println("y");
+       // }
+       // else {
+       //     System.out.println("N");
+      //  }
+        System.out.println("For Logical OR,,,");
+        if(a || b) {
+            System.out.println("Y");
+        }else {
+            System.out.println("N");
+        }
+        System.out.println("For Logical NOT");
+        System.out.print("NOT(a) is");
+        System.out.println(!a);
+        System.out.println("Not(b) is ");
+        System.out.println(!b);
+
+    }
+
+}
