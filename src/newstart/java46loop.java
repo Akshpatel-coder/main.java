4c29cafc3e96110f4d3d292fd2b912aaccae7c0d
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-06-09 11:17:10 +0530

    java46 loop

---
+++ src/newstart/java46loop.java
@@ -0,0 +1,24 @@
+package newstart;
+
+import java.sql.SQLOutput;
+
+public class java46loop {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+        System.out.println("With using a loop:");
+        int i = 1;
+        while (i <= 3) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish Running While Loop!");
+
+      //  while(true) {
+        //      System.out.println("I am an infinite while loop!");
+      //  }
+
+    }
+}
