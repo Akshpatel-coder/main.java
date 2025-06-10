ca0ef074ca9d1891e2b10df5ea395d6aa0625c29
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-06-10 11:19:15 +0530

    java51 array

---
+++ src/newstart/java51.java
@@ -0,0 +1,24 @@
+package newstart;
+
+public class java51 {
+    public static void main(String[] args) {
+       // {ARRAY}
+     /*
+     int [] marks= new int[5];
+
+     //2nd
+        int[] marks;
+        marks= new int[5];
+        marks[0] = 99;
+        marks[1] = 79;
+        marks[2] = 83;
+        marks[3] = 87;
+        marks[4] = 75;
+        System.out.println(marks[4]);
+       */
+        //3. declaration , mamory allocation and intialization together
+        int [] marks = {98, 78 , 89, 76, 87};
+        System.out.println(marks[4]);
+    }
+
+}
