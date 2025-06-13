8543081416028c1b1f8bce7c3aa67c65494f2a9f
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-06-13 10:57:09 +0530

    java62 class

---
+++ src/newstart/java62class.java
@@ -0,0 +1,42 @@
+package newstart;
+
+class Employee{
+    int id;
+    int salary;
+    String name;
+     public void printingDetails() {
+         System.out.println("My id is " + id);
+         System.out.println("My salary  is " + salary);
+         System.out.println("And my name is "+ name);
+     }
+    // public int getSalary() {
+    //     return  salary;
+ //    }
+}
+
+public class java62class {
+    public static void main(String[] args) {
+        System.out.println("This is our custom class");
+        Employee Aksh = new Employee();
+        Employee Alex = new Employee();
+        // Setting Attributes for Aksh
+        Aksh.id = 12;
+        Aksh.salary = 80;
+        Aksh.name = "AkshPatel";
+
+        // Setting Attribute for Alex
+        Alex.id = 19;
+        Alex.salary = 50;
+        Alex.name = "AlexWarner";
+
+
+        // Printing the Attributes
+        Aksh.printingDetails();
+        Alex.printingDetails();
+      //  int salary = Aksh.getSalary();
+      //  System.out.println(salary);
+      //  System.out.println(Aksh.id);
+      //  System.out.println(Aksh.name);
+
+    }
+}
