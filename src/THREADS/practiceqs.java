f10aa6db8f5324a23a9e7b68eaf570e385881700
Akshpatel-coder <akshgadhethariya@gmail.com>
2025-06-27 15:48:28 +0530

    pc2

--- src/THREADS/practiceqs.java
+++ src/THREADS/practiceqs.java
@@ -2,15 +2,29 @@ package THREADS;
 class Practice1  extends Thread{
    public  void run(){
         while(true){
-            System.out.println("Good Morning!");
+            try{
+                Thread.sleep(200);
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+            System.out.println("good morning ");
+        }
+
+
         }
     }
-}
+
 class Practice2 extends  Thread {
     public void run() {
         while (true) {
-
-            System.out.println("Good Morning");
+try{
+    Thread.sleep(200);
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+            System.out.println("welcome ");
         }
     }
 }
@@ -18,8 +32,12 @@ public class practiceqs {
     public static void main(String[] args) {
         Practice1 p1 = new Practice1();
         Practice2 p2 = new Practice2();
-        p1.start();
-        p2.start();
+        p1.setPriority(6);
+        p2.setPriority(6);
+        System.out.println(p1.getPriority());
+        System.out.println(p2.getPriority());
+      //  p1.start();
+      //  p2.start();
     }
 
 
